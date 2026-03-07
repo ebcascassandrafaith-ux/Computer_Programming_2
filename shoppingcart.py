96f89773d79c8e78161de393811b20dad75964e6
ebcascassandrafaith-ux <ebcas.cassandrafaith@gmail.com>
2026-03-07 15:51:02 +0800

    changenameAttack

---
+++ shoppingcart.py
@@ -0,0 +1,10 @@
+class Item: 
+ def __init__(product, itemID, name, price):
+    product.itemID = itemID
+    product.name = name
+    product.price = price
+
+class Shoppingcart():
+  def addItem()
+    
+    
