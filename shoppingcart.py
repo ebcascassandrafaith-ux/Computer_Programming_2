184406ca2073f0ce7166d969a025d108b9273f50
ebcascassandrafaith-ux <ebcas.cassandrafaith@gmail.com>
2026-03-10 01:30:44 +0800

    done check out

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,10 +1,62 @@
-class Item: 
- def __init__(product, itemID, name, price):
-    product.itemID = itemID
-    product.name = name
-    product.price = price
+class Cart:
+  def __init__(product):
+     product.cart = []
 
-class Shoppingcart():
-  def addItem()
-    
-    
+  def addItem(product):
+    item = input("Enter item you want to add: ")
+    product.cart.append(item)
+    print(f"{item} added to cart")
+
+  def removeItem(product):
+    removeYarn = input("Enter item you want to remove: ")
+    if removeYarn in product.cart:
+     product.cart.remove(removeYarn)
+     print(f"{removeYarn} has been successfully deleted!")
+    else:
+     print(f"{removeYarn} not found in cart")
+
+  def viewItem(product):
+   if not product.cart:
+    print("Empty Cart")
+
+   else: 
+    print("--- Your Cart ---")
+    for idx, item in enumerate(product.cart, 1):
+      print(f"{idx}.) {item}")
+
+  
+  def checkOut(product):
+   if not product.cart:
+    print("Empty Cart")
+    return
+   product.viewItem()
+   print("Payment has been processed. Done Check Out. Thank you!")
+   product.cart = []
+
+cart = Cart()
+
+while True: 
+  
+ print("---Your shopping cart---")
+ print("Main menu")
+ print("A. Add to Cart")
+ print("B. Remove Cart")
+ print("C. View Cart")
+ print("D. Check out na yarn")
+ print("E. Exit Cart")
+ choice = input("Enter letter: ").upper()
+
+ if choice == "A":
+   cart.addItem()
+ elif choice == "B":
+   cart.removeItem()
+ elif choice == "C":
+   cart.viewItem()
+ elif choice == "D":
+   cart.checkOut()
+ elif choice == "E":
+   exit(0)
+ else:
+   print("Invalid input")
+
+
